5ae82ffc690c925dcae2bc3f2b8e1df104b349d7
Z-JaDe <785457266@qq.com>
2016-11-29 18:06:56 +0800

    小修改

--- Extensions/UIButton.swift
+++ Extensions/UIButton.swift
@@ -5,4 +5,10 @@ extension UIButton {
     func setBackgroundColor(_ color: UIColor?, forState state: UIControlState) {
         self.setBackgroundImage(UIImage.imageWithColor(color), for: state)
     }
+    func changeToTemplate(isTemplate:Bool = true,color:UIColor?) {
+        var image = self.imageView?.image
+        image = isTemplate ? image?.templateImage : image?.originalImage
+        self.setImage(image, for: .normal)
+        self.tintColor = color
+    }
 }
