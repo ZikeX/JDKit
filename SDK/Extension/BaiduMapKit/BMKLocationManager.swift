502f4f1a2989acd3f2bddf9795b2a2d4e2f1f34f
Z-JaDe <785457266@qq.com>
2016-12-28 18:28:21 +0800

    定位功能

---
+++ SDK/Extension/BaiduMapKit/BMKLocationManager.swift
@@ -0,0 +1,136 @@
+//
+//  BMKLocationManager.swift
+//  ZiWoYou
+//
+//  Created by 茶古电子商务 on 16/12/28.
+//  Copyright © 2016年 Z_JaDe. All rights reserved.
+//
+
+import Foundation
+import PermissionScope
+import RxSwift
+
+class BMKLocationManager:NSObject {
+    // MARK: 反编码 坐标转地址
+    lazy var geoCodeSearch = BMKGeoCodeSearch()
+    fileprivate lazy var reverseGeoCodeSubject = ReplaySubject<BMKReverseGeoCodeResult>.create(bufferSize: 1)
+    // MARK: 定位
+    lazy var locationService = BMKLocationService()
+    fileprivate lazy var locationSubject = ReplaySubject<BMKUserLocation>.create(bufferSize: 1)
+    
+}
+// MARK: - 反编码 坐标转地址
+extension BMKLocationManager {
+    func locationAndReverseGeoCode() -> Observable<BMKReverseGeoCodeResult> {
+        return self.getLocation().flatMap {[unowned self] (location) -> Observable<BMKReverseGeoCodeResult> in
+            return self.reverseGeoCode(location.location.coordinate)
+        }
+    }
+    func reverseGeoCode(_ coordinate:CLLocationCoordinate2D) -> Observable<BMKReverseGeoCodeResult> {
+        self.beginSearch(coordinate)
+        return self.reverseGeoCodeSubject.take(1)
+    }
+    fileprivate func beginSearch(_ coordinate:CLLocationCoordinate2D) {
+        self.geoCodeSearch.delegate = self
+        let result = BMKReverseGeoCodeOption()
+        result.reverseGeoPoint = coordinate
+        if self.geoCodeSearch.reverseGeoCode(result) == false {
+            logDebug("反geo检索发送失败")
+        }
+    }
+    func endSearch() {
+        geoCodeSearch.delegate = nil
+    }
+}
+// MARK: - 反编码 坐标转地址 BMKGeoCodeSearchDelegate
+extension BMKLocationManager:BMKGeoCodeSearchDelegate {
+    /// ZJaDe: 地址信息搜索结果
+    func onGetGeoCodeResult(_ searcher: BMKGeoCodeSearch!, result: BMKGeoCodeResult!, errorCode error: BMKSearchErrorCode) {
+        
+    }
+    /// ZJaDe: 返回反地理编码搜索结果
+    func onGetReverseGeoCodeResult(_ searcher: BMKGeoCodeSearch!, result: BMKReverseGeoCodeResult!, errorCode error: BMKSearchErrorCode) {
+        if result.address != nil, result.address.length > 0 {
+            self.reverseGeoCodeSubject.onNext(result)
+            self.reverseGeoCodeSubject.onCompleted()
+        }else if (error != BMK_SEARCH_NO_ERROR) {
+            HUD.showError("反编码错误->\(error)")
+        }
+        self.endSearch()
+    }
+}
+// MARK: - 定位
+extension BMKLocationManager {
+    func getLocation() -> Observable<BMKUserLocation> {
+        return self.location().take(1)
+    }
+    // MARK: 开启定位同时验证
+    func location() -> Observable<BMKUserLocation> {
+        self.checkCanLocation {
+            self.startUserLocationService()
+        }
+        return self.locationSubject.do(onDispose: {
+            self.endLocation()
+            
+        })
+    }
+    // MARK: 只请求定位，不提示错误
+    func onlyLocation() -> Observable<BMKUserLocation> {
+        self.startUserLocationService()
+        return self.locationSubject.do(onDispose: {
+            self.endLocation()
+        })
+    }
+    // MARK: 停止定位
+    func endLocation() {
+        self.stopUserLocationService()
+    }
+    // MARK: - 
+    fileprivate func startUserLocationService() {
+        if self.locationService.delegate == nil {
+            self.locationService.delegate = self
+            self.locationService.startUserLocationService()
+        }
+    }
+    fileprivate func stopUserLocationService() {
+        self.locationService.stopUserLocationService()
+        self.locationService.delegate = nil
+        
+    }
+    fileprivate func checkCanLocation(_ closure:@escaping ()->()) {
+        let pscope = PermissionScope()
+        pscope.addPermission(LocationWhileInUsePermission(), message: "如果拒绝将无法使用定位功能")
+        pscope.bodyLabel.text = "在您定位之前，app需要获取\r\niPhone的定位权限"
+        pscope.show({ (finished, results) in
+            closure()
+        }, cancelled: {(results) in
+        })
+    }
+    // MARK: 更新定位
+    fileprivate func updateLocation(_ userLocation:BMKUserLocation) {
+        if let coordinate = userLocation.location?.coordinate {
+            UserInfo.shared.personModel.coordinate = coordinate
+            self.locationSubject.onNext(userLocation)
+        }
+    }
+}
+// MARK: - 定位 BMKLocationServiceDelegate
+extension BMKLocationManager:BMKLocationServiceDelegate {
+    func willStartLocatingUser() {
+        logInfo("开始定位")
+    }
+    func didStopLocatingUser() {
+        logInfo("停止定位")
+    }
+    
+    func didUpdate(_ userLocation: BMKUserLocation!) {
+        updateLocation(userLocation)
+    }
+    func didUpdateUserHeading(_ userLocation: BMKUserLocation!) {
+        updateLocation(userLocation)
+    }
+    func didFailToLocateUserWithError(_ error: Error!) {
+        HUD.showError("定位出现位置错误")
+        self.locationSubject.onError(error)
+    }
+}
--- SDK/Extension/BaiduMapKit/BaseMapViewController.swift
+++ SDK/Extension/BaiduMapKit/BaseMapViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class BaseMapViewController: BaseViewController {
+    lazy var searchView:UIView = UIView()
+    lazy var searchTextField:SearchTextField = SearchTextField(textFieldType: .lightWhiteSearchBarWithCity)
+    /// ZJaDe: 
     var headerShadowView:UIView = {
         let view = UIView()
         let gradientLayer = CAGradientLayer()
@@ -20,26 +23,65 @@ class BaseMapViewController: BaseViewController {
         return view
     }()
     lazy var mapVC:BaseMapScrollViewController = BaseMapScrollViewController()
-    
     var mapView:BMKMapView {
         return self.mapVC.mapView
     }
+    var currentLocation:BMKUserLocation?
+    private var needUpdateMyLocation = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addChildScrollVC(edgesToFill: true)
         self.view.addSubview(self.headerShadowView)
+        
+        self.navBarAlpha = 0
+        self.navTintColor = Color.white
+        self.backButton.addShadowInWhiteView()
+        self.configNavBarItem { (navItem) in
+            navItem.leftBarButtonItem = self.backButton.barButtonItem()
+            navItem.titleView = self.searchView
+        }
+        configSearchView()
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         mapView.delegate = self
+        self.setNeedRetryRequest()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    override func request() {
+        BMKLocationManager().getLocation().subscribe(onNext:{[unowned self] (userLocation) in
+            if self.needUpdateMyLocation {
+                self.needUpdateMyLocation = false
+                self.currentLocation = userLocation
+                self.updateData()
+            }
+        }).addDisposableTo(disposeBag)
+    }
+    override func updateData() {
+        self.mapView.showsUserLocation = true
+        self.mapView.updateLocationData(self.currentLocation)
+        self.mapView.userTrackingMode = BMKUserTrackingModeFollow
     }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         mapView.delegate = nil
     }
 }
-
+extension BaseMapViewController {
+    // MARK: searchView
+    func configSearchView() {
+        searchTextField.text = "   目的地、景点、酒店、"
+        searchView.addSubview(searchTextField)
+        searchView.size = searchTextField.intrinsicContentSize
+        searchTextField.edgesToView()
+        _ = searchView.rx.whenTouch({ (view) in
+            // TODO: 点击搜索框时
+        })
+    }
+}
 extension BaseMapViewController:AddChildScrollProtocol {
     func createScrollVC(index: Int) -> BaseMapScrollViewController {
         return self.mapVC
